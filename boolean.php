09583efb8b1ed247850ba29a5d919f4adf5c1c2a
Eka Zainu <122343223+Engkokk@users.noreply.github.com>
2023-09-11 09:35:35 +0700

    Add files via upload

---
+++ boolean.php
@@ -0,0 +1,18 @@
+<?php
+    echo "___Tipe Data Boolean___";
+    echo "<br>";
+    $menang = true;
+    if ($menang == true){
+        echo "Selamat Anda Menang!";
+    }   else{
+        echo "Maaf, Anda Kalah....";
+    }
+    echo "<br>";
+    $menang = true;
+    if ($menang == false){
+        echo "Selamat Anda Menang!";
+    }   else{
+        echo "Maaf, Anda Kalah....";
+    }
+    echo "<br>";
+?>
