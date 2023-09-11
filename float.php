0ce7e4092b6cd30fdee57e7a2181822f9eece2f0
Eka Zainu <122343223+Engkokk@users.noreply.github.com>
2023-09-11 09:01:28 +0700

    Add files via upload

---
+++ float.php
@@ -0,0 +1,12 @@
+<?php
+    echo "___Tipe Data Float___";
+    echo "<br>";
+    $a = 5.234; // desimal
+    $b = 3.2e4; // 3.2 x 10^4
+    $c = 4E-10; // 4 x 10^(-10) = 0,00000
+    var_dump($a);
+    echo "<br>";
+    var_dump($b);
+    echo "<br>";
+    var_dump($c);
+    echo "<br><br>";
